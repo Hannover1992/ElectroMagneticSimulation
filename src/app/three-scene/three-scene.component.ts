1d2eed3d5811b833af98928805ebc80a4cb3ce23
Patryk Krzyzanski <hannover1992@googlemail.com>
2024-01-05 22:53:53 +0100

    refactor

--- src/app/three-scene/three-scene.component.ts
+++ src/app/three-scene/three-scene.component.ts
@@ -10,30 +10,43 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Im
   templateUrl: './three-scene.component.html',
   styleUrls: ['./three-scene.component.css']
 })
+
 export class ThreeSceneComponent implements AfterViewInit {
   @ViewChild('myCanvas') private canvasRef!: ElementRef;  // Notice the "!" after "ElementRef"
-    private scene!: THREE.Scene;
-    private camera!: THREE.OrthographicCamera;
-    private renderer!: THREE.WebGLRenderer;
+
+  private scene!:     THREE.Scene;
+  private camera!:    THREE.OrthographicCamera;
+  private renderer!:  THREE.WebGLRenderer;
+
 
   constructor(){
+    this.initScene();
+    this.initCamera();
+    this.preInitRenderer();
+  }
+
+  initScene() {
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0x000000);
-    this.initCamera();
+  }
+
+  initCamera() {
+    const aspect  =   calculateRatio();   // const camera    = new THREE.OrthographicCamera(75, aspect, 0.1, 1000);
+    const h       =   100; // frustum height
+
+    this.camera = new THREE.OrthographicCamera( - h * aspect / 2, h * aspect / 2, h / 2, - h / 2, 1, 1000 );
+    this.camera.position.set( - 64, - 64, 128 );
+    this.camera.up.set( 0, 0, 1 ); // In our data, z is up
+  }
+
+  preInitRenderer(){
+    this.renderer  = new THREE.WebGLRenderer();
   }
 
 
   ngAfterViewInit(): void {
 
-
-
-    this.renderer  = new THREE.WebGLRenderer({
-      canvas: this.canvasRef.nativeElement, antialias: true
-    });
-
-
-    this.renderer.setPixelRatio( window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth*0.98, window.innerHeight * 0.98);
+    this.postInitRender();
 
     this.camera.position.setZ(30);
     this.camera.position.setX(-30);
@@ -54,47 +67,49 @@ export class ThreeSceneComponent implements AfterViewInit {
 
     controls.addEventListener('change', this.render); // use if there is no animation loop
 
-    controls.minDistance = 20;
-    controls.maxDistance = 500;
-    controls.enablePan = true;
+      controls.minDistance = 20;
+      controls.maxDistance = 500;
+      controls.enablePan = true;
 
 
 
-    // Render-Schleife
-    const animate = () => {
-      requestAnimationFrame(animate);
+      // Render-Schleife
+      const animate = () => {
+        requestAnimationFrame(animate);
 
-      torus.rotation.x += 0.01;
-      torus.rotation.y += 0.005;
-      torus.rotation.z += 0.01;
+        torus.rotation.x += 0.01;
+        torus.rotation.y += 0.005;
+        torus.rotation.z += 0.01;
 
-      // Hier könnten Sie die Logik für Bewegungen oder Interaktionen hinzufügen
+        // Hier könnten Sie die Logik für Bewegungen oder Interaktionen hinzufügen
 
-      this.renderer.render(this.scene, this.camera);
-      controls.update();
-      // animate();
-      console.log("animating");
-    };
-    animate();
+        this.renderer.render(this.scene, this.camera);
+        controls.update();
+        // animate();
+        console.log("animating");
+      };
+      animate();
+  }
+
+  postInitRender() {
+    this.renderer  = new THREE.WebGLRenderer({
+      canvas: this.canvasRef.nativeElement, antialias: true
+    });
+
+    this.renderer.setPixelRatio( window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth*0.98, window.innerHeight * 0.98);
   }
 
 
   render() {
-    this.renderer.render(this.scene, this.camera);
+    if(this.renderer){
+      this.renderer.render(this.scene, this.camera);
+    }
   }
 
-
-  calculateRAtio() {
-    return window.innerHeight / window.innerWidth;
-  }
-
-  initCamera() {
-    const aspect    =  this.calculateRAtio();   // const camera    = new THREE.OrthographicCamera(75, aspect, 0.1, 1000);
-    const h = 100; // frustum height
-
-    this.camera = new THREE.OrthographicCamera( - h * aspect / 2, h * aspect / 2, h / 2, - h / 2, 1, 1000 );
-    this.camera.position.set( - 64, - 64, 128 );
-    this.camera.up.set( 0, 0, 1 ); // In our data, z is up
-  }
 
 }
+
+  function calculateRatio() {
+    return window.innerHeight / window.innerWidth;
+  }
