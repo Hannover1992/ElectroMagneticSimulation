d539f4759e285e8e12583933e24de7029827b55c
Patryk Krzyzanski <hannover1992@googlemail.com>
2024-01-06 12:02:53 +0100

    baby steps rendrer

---
+++ src/app/three-scene/renderer.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, ElementRef} from '@angular/core';
+import { WebGLRenderer } from 'three/src/renderers/WebGLRenderer';
+import { CameraService } from './camera.service';
+import { SceneService } from './scene.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RendererService {
+
+  private renderer!:  WebGLRenderer;
+
+  constructor(
+    private sceneService    :SceneService,
+    private cameraService   :CameraService,
+  ) {
+    this.renderer  =new WebGLRenderer();
+
+    this.renderer.setPixelRatio( window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth*0.98, window.innerHeight * 0.98);
+  }
+
+  render() {
+    if(this.renderer){
+      this.renderer.render(this.sceneService.scene, this.cameraService.camera);
+    }
+  }
+
+  initRenderer(canvas: ElementRef<HTMLCanvasElement>){
+    this.renderer  = new WebGLRenderer({
+      canvas: canvas.nativeElement, antialias: true
+    });
+  }
+
+  getDom(): HTMLElement {
+    return this.renderer.domElement
+  }
+}
--- src/app/three-scene/three-scene.component.ts
+++ src/app/three-scene/three-scene.component.ts
@@ -1,9 +1,9 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Import OrbitControls
 import { GeometryService } from './geometry.service';
 import { SceneService } from './scene.service';
 import { CameraService } from './camera.service';
+import { RendererService } from './renderer.service';
 
 
 @Component({
@@ -18,26 +18,23 @@ import { CameraService } from './camera.service';
 export class ThreeSceneComponent implements AfterViewInit {
   @ViewChild('myCanvas') private canvasRef!: ElementRef;  // Notice the "!" after "ElementRef"
 
-  private renderer!:  THREE.WebGLRenderer;
   controls!: OrbitControls;
 
   constructor(
     private geometryService :GeometryService,
     private sceneService    :SceneService,
-    private cameraService   :CameraService
+    private cameraService   :CameraService,
+    private rendererService :RendererService
   ){
-    this.preInitRenderer();
   }
 
 
 
-  preInitRenderer(){
-    this.renderer  = new THREE.WebGLRenderer();
-  }
 
 
   ngAfterViewInit(): void {
-    this.postInitRender();
+    this.rendererService.initRenderer(this.canvasRef);
+
     this.postInitControl();
 
     this.animate();
@@ -49,21 +46,13 @@ export class ThreeSceneComponent implements AfterViewInit {
   }
 
   initControls() {
-    this.controls = new OrbitControls(this.cameraService.camera, this.renderer.domElement);
+    this.controls = new OrbitControls(this.cameraService.camera, this.rendererService.getDom());
     this.controls.addEventListener('change', this.render); // use if there is no animation loop
     this.controls.minDistance = 20;
     this.controls.maxDistance = 500;
     this.controls.enablePan = true;
   }
 
-  postInitRender() {
-    this.renderer  = new THREE.WebGLRenderer({
-      canvas: this.canvasRef.nativeElement, antialias: true
-    });
-
-    this.renderer.setPixelRatio( window.devicePixelRatio);
-    this.renderer.setSize(window.innerWidth*0.98, window.innerHeight * 0.98);
-  }
 
   animate = () => {
     requestAnimationFrame(this.animate);
@@ -74,11 +63,6 @@ export class ThreeSceneComponent implements AfterViewInit {
       console.log("animating");
   };
 
-  render() {
-    if(this.renderer){
-      this.renderer.render(this.sceneService.scene, this.cameraService.camera);
-    }
-  }
 
 }
 
